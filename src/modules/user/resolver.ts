a82cbaef33315fdfb378a730849a181a51487bf4
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-09-30 14:25:39 +0530

    added errors to user-releated queries and mutations

--- src/modules/user/resolver.ts
+++ src/modules/user/resolver.ts
@@ -1,5 +1,6 @@
 import { Resolver, Arg, Query, Mutation } from "type-graphql";
 import * as bcrypt from "bcrypt";
+import * as _ from "lodash";
 import * as fe from "easygraphql-format-error";
 
 import { User } from "./entity";
@@ -11,19 +12,34 @@ const FormatError: fe = new fe();
 export class UserResolver {
   @Query(() => [User])
   async users(): Promise<User[]> {
-    return User.find({});
+    const users: User[] = await User.find({});
+    if (_.isEmpty(users)) {
+      throw new Error(FormatError.errorName.NOT_FOUND);
+    }
+
+    return users;
   }
 
   @Query(() => User)
   async user(@Arg("id", { nullable: true }) id?: string): Promise<User> {
     const user: User | undefined = await User.findOne(id);
+    if (_.isEmpty(user)) {
+      throw new Error(FormatError.errorName.NOT_FOUND);
+    }
     return user;
   }
 
   @Mutation(() => User)
   async register(@Arg("input") input: UserRegistrationInput): Promise<User> {
-    const user: User = await User.create(input).save();
+    const existing: User[] = await User.find({
+      where: { username: input.username },
+      select: ["id"]
+    });
+    if (!_.isEmpty(existing)) {
+      throw new Error(FormatError.errorName.CONFLICT);
+    }
 
+    const user: User = await User.create(input).save();
     return user;
   }
 
@@ -32,9 +48,12 @@ export class UserResolver {
     const user: User | undefined = await User.findOne({
       where: { username: input.username }
     });
+    if (_.isEmpty(user)) {
+      throw new Error(FormatError.errorName.NOT_FOUND);
+    }
     const valid: boolean = await bcrypt.compare(input.password, user.password);
     if (!valid) {
-      throw new Error("Invalid username/password");
+      throw new Error(FormatError.errorName.UNAUTHORIZED);
     }
 
     return user;
